b6db38ef93e2b0fae3121ebf723a5644ab605de2
Shreyansh Patel <shreypatel13579@gmail.com>
2022-01-18 20:49:49 +0530

    Added heroku config

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const viewRouter = require('./routes/viewRoutes');
 // Start express application
 const app = express();
 
+app.enable('trust proxy');
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
